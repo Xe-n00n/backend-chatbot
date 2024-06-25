a9fd3550bb47989d5f0878ab870d867af5199ac2
Xe-n00n <ledra.hind@gmail.com>
2024-06-25 22:36:48 +0100

    removed cors content

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 
 // Middlewares
 
-app.use(cors({origin:"*", credentials: true}));
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan("dev")); // for development
